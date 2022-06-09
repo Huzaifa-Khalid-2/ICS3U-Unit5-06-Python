f0f02da463adb0e6fcccecdecd53af7b5105702e
Ubuntu <ubuntu@ip-172-31-21-22.ec2.internal>
2022-06-09 18:29:09 +0000

    added code

--- format.py
+++ format.py
@@ -1,24 +1,32 @@
 #!/usr/bin/env python3
 
-# Created by: Mr. Coxall
-# Created on: Sep 2020
-# This program shows formatting output
+# Created by: Huzaifa Khalid
+# Created on: June 2022
+# This program uses a function by reference
+
+import math
+
+
+def rounder(someVariable, amount):
+    # function adds 1, by reference
+
+    ten_times = 10**amount
+    someVariable[0] = someVariable[0] * ten_times
+    someVariable[0] = someVariable[0] + 0.5
+    someVariable[0] = int(someVariable[0])
+    someVariable[0] = someVariable[0] / ten_times
 
 
 def main():
-    # this function shows formatting output
+    # this function gets a number and calls the add_one function
 
+    someNumber = []
     # input
-    value = float(input("Enter a value ($): "))
-
-    # process
-    # nill
-
-    # output
-    print("")
-    print("The cost is: ${:,.2f}".format(value))
-
-    print("\nDone.")
+    number_round = float(input("Enter a number to round: "))
+    amount_round = int(input("Enter how many decimal places to round by: "))
+    someNumber.append(number_round)
+    rounder(someNumber, amount_round)
+    print("\nThe rounded number is: {0}".format(someNumber[0]))
 
 
 if __name__ == "__main__":
